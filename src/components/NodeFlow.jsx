e3cca0bc37af34940a8a4ef13c110a066933bd65
Venkatesh Prasad <venkatesh.prasad@adroit-vantage.com>
2024-06-18 11:43:57 +0530

    code clean up done

--- src/components/NodeFlow.jsx
+++ src/components/NodeFlow.jsx
@@ -5,7 +5,7 @@ import "reactflow/dist/style.css";
 const NodeFlow = ({ nodes, edges, nodeTypes }) => {
     return (
         <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes} fitView={false} minZoom={0.1}
-            maxZoom={1.2} defaultViewport={{ x: 20, y: 20, zoom: 0.5 }}>
+            maxZoom={1.2} defaultViewport={{ x: 20, y: 20, zoom: 1 }}>
             <Background />
             <Controls showInteractive={false} />
             <MiniMap pannable zoomable style={{ background: "purple" }} />
